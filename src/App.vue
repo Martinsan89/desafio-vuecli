9d2b145b9baf050267002513b44d1819dab856cb
marsan89 <martinsanchez89@gmail.com>
2022-05-12 16:07:54 -0300

    Desafio-Vuecli

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,96 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+     <DesafioTablas
+        :titulos="tabla1.titulos"
+        :registros="tabla1.registros"
+        :clases="tabla1.clases"
+        :indice="tabla1.indice"
+      />
+      <DesafioTablas
+        :titulos="tabla2.titulos2"
+        :registros="tabla2.registros2"
+        :clases="tabla2.clases2"
+        :indice="tabla2.indice2"
+      />
+      <DesafioTablas
+        :titulos="tabla3.titulos3"
+        :registros="tabla3.registros3"
+        :clases="tabla3.clases3"
+        :indice="tabla3.indice3"
+      />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DesafioTablas from './components/DesafioTablas.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    DesafioTablas
+  },
+  data(){
+     return{
+        tabla1: {
+                titulos: ["ID", "Nombre", "Apellido", "Provincia"],
+                registros: [
+                    ["Jose", "Velez", "San Luis"],
+                    ["Ramon", "Gomez", "Santa Fe"],
+                    ["Marcelo", "Godino", "Buenos Aires"],
+                    ["Ramiro", "Gimenez", "Salta"],
+                ],
+                clases: {
+                    "table-dark": true,
+                    "text-secondary": true,
+                },
+                indice: {
+                    "background-color": "cadetblue",
+                    "color": "black",
+                },
+        },
+        tabla2: {
+            titulos2: ["ID", "Nombre", "Apellido", "Provincia"],
+            registros2: [
+                ["Celso", "Fernandez", "La Pampa"],
+                ["Matias", "Perez", "Formosa"],
+                ["Marcela", "Garcia", "Chaco"],
+                ["Omar", "Ramirez", "Misiones"],
+            ],
+            clases2: {
+                "table-info": true,
+                "text-success": true,
+            },
+            indice2: {
+                "background-color": "black",
+                "color": "white",
+            },
+        },
+        tabla3: {
+            titulos3: ["ID", "Nombre", "Apellido", "Provincia"],
+            registros3: [
+                ["Ernesta", "Resta", "Chubut"],
+                ["Joaquin", "Morales", "Rio Negro"],
+                ["Roberto", "Sanchez", "Tucuman"],
+                ["Debora", "Esquer", "Tierra del fuego"],
+            ],
+            clases3: {
+                "table-danger": true,
+                "text-primary": true,
+            },
+            indice3: {
+                "background-color": "brown",
+                "color": "white",
+            },
+        }
+      }
+  } 
 }
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+ width: 80%;
+ margin: 1rem auto;
 }
 </style>
---
+++ src/components/DesafioTablas.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <table class="table" :class="clases">
+        <thead  >
+            <tr>
+                <th :style="indice" v-for="(item, i) in titulos" :key="i" scope="col">{{ item }}</th>
+            </tr>
+        </thead>
+        <tbody>
+             <tr v-for="(registro, i) in registros" :key="i">
+               <th>{{ i + 1 }}</th>
+                <th v-for="(row, r) in registro" :key="r">{{row}}</th>
+              </tr>
+         </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        titulos: {
+        type: Array,
+        required: true,
+        },
+        registros: {
+        type: Array,
+        required: true
+        },
+        clases: {
+        type: Object,
+        required: true
+        },
+        indice: {
+        type: Object,
+        required: true
+        }
+    },
+}
+</script>
+
